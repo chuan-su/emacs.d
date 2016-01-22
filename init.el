9c2f5e3b4fdedd94990924060c3d0fc8daf3e855
Chuan <chuansu@icloud.com>
2016-01-22 21:37:49 +0100

    comment out erlang for now

--- init.el
+++ init.el
@@ -112,16 +112,16 @@
 (eval-after-load 'web-mode
   '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
 
-(setq load-path (cons"/usr/local/Cellar/erlang/17.5/lib/erlang/lib/tools-2.7.2/emacs" load-path))
-(setq erlang-root-dir "/usr/local/Cellar/erlang/17.5/lib/erlang/lib")
-(setq exec-path (cons "/usr/local/Cellar/erlang/17.5/lib/erlang/bin" exec-path))
-(require 'erlang-start)
+;;(setq load-path (cons"/usr/local/Cellar/erlang/17.5/lib/erlang/lib/tools-2.7.2/emacs" load-path))
+;;(setq erlang-root-dir "/usr/local/Cellar/erlang/17.5/lib/erlang/lib")
+;;(setq exec-path (cons "/usr/local/Cellar/erlang/17.5/lib/erlang/bin" exec-path))
+;;(require 'erlang-start)
 
-(defun customizations-for-erlang-mode ()
-  (linum-mode t)  
-)
+;;(defun customizations-for-erlang-mode ()
+  ;;(linum-mode t)  
+;;)
 
-(add-hook 'erlang-mode-hook 'customizations-for-erlang-mode)
+;;(add-hook 'erlang-mode-hook 'customizations-for-erlang-mode)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
