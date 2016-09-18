fe7e91e596b717fc3f9cab2a7e7b660078f33e28
Chuan Su <chuansu@icloud.com>
2016-09-18 17:25:35 +0200

    flx-ido

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 
 
 ;;list the packages you want
-;;(setq package-list '(ac-js2 auto-complete auto-indent-mode smex ido-ubiquitous ido-vertical-mode dockerfile-mode
+;;(setq package-list '(ac-js2 auto-complete auto-indent-mode smex flx-ido ido-ubiquitous ido-vertical-mode dockerfile-mode
  ;;flycheck js2-mode php-mode rvm  magit move-text projectile projectile-rails project-explorer zenburn-theme  web-mode yaml-mode evil-nerd-commenter multiple-cursors smartparens
  ;;markdown-mode restclient rainbow-delimiters
 ;;))
@@ -32,6 +32,10 @@
 (require 'ido)
 (ido-mode t)
 (ido-everywhere t)
+(require 'flx-ido)
+(flx-ido-mode t)
+(setq ido-enable-flex-matching t)
+(setq ido-use-faces nil)
 (require 'ido-ubiquitous)
 (ido-ubiquitous-mode t)
 (require 'ido-vertical-mode)
@@ -174,12 +178,12 @@
  '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
-    ("316d29f8cd6ca980bf2e3f1c44d3a64c1a20ac5f825a167f76e5c619b4e92ff4" "19553e40ff93a241cdfce090fd60e1420e0b371eec1d134a0da21f6f6488a74a" "b84c377b535f71f00e76bfc7ea8c1dcf75c459348292b01ed79e2916fb6451f8" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "37def0fac11a4890922af9febc8394e3b6e3c68904a294a2d440b1904e979c7e" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "f0d8af755039aa25cd0792ace9002ba885fd14ac8e8807388ab00ec84c9497d7" "68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "fa11f855b5f606f84e50106a7360c72aac88fee5f6fb8084aa4329009b61c5a2" "49de25b465bc3c2498bcd4c1575fa0090bd56fc79cdb49b919b49eaea17ee1dd" "38ba6a938d67a452aeb1dada9d7cdeca4d9f18114e9fc8ed2b972573138d4664" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
+    ("0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" "316d29f8cd6ca980bf2e3f1c44d3a64c1a20ac5f825a167f76e5c619b4e92ff4" "19553e40ff93a241cdfce090fd60e1420e0b371eec1d134a0da21f6f6488a74a" "b84c377b535f71f00e76bfc7ea8c1dcf75c459348292b01ed79e2916fb6451f8" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "37def0fac11a4890922af9febc8394e3b6e3c68904a294a2d440b1904e979c7e" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "b571f92c9bfaf4a28cb64ae4b4cdbda95241cd62cf07d942be44dc8f46c491f4" "f0d8af755039aa25cd0792ace9002ba885fd14ac8e8807388ab00ec84c9497d7" "68d36308fc6e7395f7e6355f92c1dd9029c7a672cbecf8048e2933a053cf27e6" "fa11f855b5f606f84e50106a7360c72aac88fee5f6fb8084aa4329009b61c5a2" "49de25b465bc3c2498bcd4c1575fa0090bd56fc79cdb49b919b49eaea17ee1dd" "38ba6a938d67a452aeb1dada9d7cdeca4d9f18114e9fc8ed2b972573138d4664" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "72a81c54c97b9e5efcc3ea214382615649ebb539cb4f2fe3a46cd12af72c7607" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" default)))
  '(fringe-mode (quote (nil . 0)) nil (fringe))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (restclient markdown-mode smartparens multiple-cursors evil-nerd-commenter yaml-mode web-mode zenburn-theme project-explorer projectile-rails projectile move-text magit rvm php-mode flycheck dockerfile-mode ido-vertical-mode ido-ubiquitous smex auto-indent-mode auto-complete ac-js2)))
+    (flx-ido restclient markdown-mode smartparens multiple-cursors evil-nerd-commenter yaml-mode web-mode zenburn-theme project-explorer projectile-rails projectile move-text magit rvm php-mode flycheck dockerfile-mode ido-vertical-mode ido-ubiquitous smex auto-indent-mode auto-complete ac-js2)))
  '(scroll-bar-mode nil)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
